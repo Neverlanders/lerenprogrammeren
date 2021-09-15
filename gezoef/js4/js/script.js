8e2b33e9404a72bca5dc692f99c726377ef06964
Tom Vanhoutte <vanhoutte.tom@gmail.com>
2021-09-15 09:32:13 +0200

    meervoudige selectie

---
+++ gezoef/js4/js/script.js
@@ -0,0 +1,24 @@
+//meervoudige selectie
+//var onderwijs = 'syntra'
+var onderwijs = prompt("Keuze");
+switch(onderwijs){
+    case 'vdab':
+        console.log('Cursus is gegeven door VDAB');
+        break;
+    case 'syntra':
+        console.log('Cursus is gegeven door Syntra');
+        break;
+    default:
+        console.log('Gegeven door een andere instelling');
+}
+//enkelvoudige selectie shorthand
+/*var x = 1;
+if(x == 1){
+    //code
+}else{
+    //code
+}
+
+x==1 ? //code : //code*/
+
+
